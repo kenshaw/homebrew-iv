262b87ad7e559c5e818fd012285e39a8031aedc6
Kenneth Shaw <kenshaw@gmail.com>
2024-04-05 23:04:57 +0000

    Update iv version to v0.2.1
    Updates dependencies and adds shell completion support
    
    **Full Changelog**: https://github.com/kenshaw/iv/compare/v0.1.5...v0.2.1

--- Formula/iv.rb
+++ Formula/iv.rb
@@ -4,8 +4,8 @@ class Iv < Formula
   desc "a command-line image viewer using terminal graphics (Sixel, iTerm, Kitty)"
   homepage "https://#{$pkg}"
   head "https://#{$pkg}.git"
-  url "https://github.com/kenshaw/iv/archive/v0.1.5.tar.gz"
-  sha256 "ca2e5ae0531c09054abf39ce75ab08d454e604b3be6e6f90938005085b167b2e"
+  url "https://github.com/kenshaw/iv/archive/v0.2.1.tar.gz"
+  sha256 "4b27efde322b18e32b98df249bed247cb183342acb4e2c018a4024adb4018c29"
 
   depends_on "go" => :build
 
