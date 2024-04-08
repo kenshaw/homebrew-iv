1e8a2d4ae6965bfedc6fcefc036f04a047f9383b
Kenneth Shaw <kenshaw@gmail.com>
2024-04-08 00:59:21 +0000

    Update iv version to v0.2.2
    Adds background support for non-svg's.
    
    **Full Changelog**: https://github.com/kenshaw/iv/compare/v0.2.1...v0.2.2

--- Formula/iv.rb
+++ Formula/iv.rb
@@ -4,8 +4,8 @@ class Iv < Formula
   desc "a command-line image viewer using terminal graphics (Sixel, iTerm, Kitty)"
   homepage "https://#{$pkg}"
   head "https://#{$pkg}.git"
-  url "https://github.com/kenshaw/iv/archive/v0.2.1.tar.gz"
-  sha256 "4b27efde322b18e32b98df249bed247cb183342acb4e2c018a4024adb4018c29"
+  url "https://github.com/kenshaw/iv/archive/v0.2.2.tar.gz"
+  sha256 "ab4351e1ad45bdef130cf118dbe0dfec8635a24caf77aaf1014ce21a066c7688"
 
   depends_on "go" => :build
 
