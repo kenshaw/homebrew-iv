a192817b12efd9cae2d97bb7cae5f60bff2d2260
Kenneth Shaw <kenshaw@gmail.com>
2026-08-11 13:36:30 +0000

    Update iv version to v0.14.1
    **Full Changelog**: https://github.com/kenshaw/iv/compare/v0.14.0...v0.14.1

--- Formula/iv.rb
+++ Formula/iv.rb
@@ -4,8 +4,8 @@ class Iv < Formula
   desc "a command-line image viewer using terminal graphics (Sixel, iTerm, Kitty)"
   homepage "https://#{$pkg}"
   head "https://#{$pkg}.git"
-  url "https://github.com/kenshaw/iv/archive/v0.14.0.tar.gz"
-  sha256 "604e38c6c7c36ac8d68df694c50be6aca8ece93a522a40738fd5a0909cbcd532"
+  url "https://github.com/kenshaw/iv/archive/v0.14.1.tar.gz"
+  sha256 "c8446c54b91eec7068a7770a437d2915f85bdd441f2166b9a8fa4e851d59daae"
 
   depends_on "go" => :build
 
